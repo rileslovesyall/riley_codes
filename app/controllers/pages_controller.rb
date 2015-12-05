37d4a83dbf0b781b39d77b1b95d9fe13c738629a
Riley Spicer <riley.r.spicer@gmail.com>
2015-12-05 12:24:26 -0800

    was commiting on wrong project bahaha, been doing css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,10 @@ class PagesController < ApplicationController
   before_action :authenticate_user!, only: [:secret]
 
   def index
-    
+    render :layout => "front-page"
   end
   def about
-    
   end
   def projects
-    
   end
 end
