3a069b30e7d303c9f6112a5f789cbf50abc49dcc
Riley Spicer <riley.r.spicer@gmail.com>
2016-01-17 19:59:23 -0800

    sketched out blog form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,36 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
 
-  def index; end
+  def index
+    @posts = Post.all
+  end
+
   def show; end
+
   def new; end
-  def create; end
+
+  def create
+    Post.create(
+        title: post_params[:title],
+        body: post_params[:body]
+      )
+    redirect_to :index
+  end
+
   def edit; end
+
   def update; end
+
   def destroy; end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
   
 end
