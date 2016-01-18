0978bc8db6d25c58f563a479295e1d5da4719e89
Riley Spicer <riley.r.spicer@gmail.com>
2016-01-17 18:37:21 -0800

    removed last bit of devise nonsense

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get '/projects' => 'pages#projects', as: :projects
   get '/about' => 'pages#about', as: :about
   get '/secret', to: 'pages#secret', as: :secret
+  get '/haml', to: 'pages#haml_about'
+
 end
